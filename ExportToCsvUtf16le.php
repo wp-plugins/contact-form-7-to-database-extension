0021ed7bc06ac25f875b7d995da81e1010c7dd6e
msimpson <msimpson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-04 02:41:00 +0000

    Added encodeWordRfc2231
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-to-database-extension/trunk@296339 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ExportToCsvUtf16le.php
+++ ExportToCsvUtf16le.php
@@ -13,10 +13,15 @@ class ExportToCsvUtf16le {
         $tableData = $plugin->getRowsPivot($formName);
 
         header("Content-Type: text/csv; charset=UTF-16LE");
-//    header("Content-Type: application/vnd.ms-excel");
         header("Expires: 0");
         header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-        header("Content-Disposition: attachment; filename=\"$formName.csv\"");
+
+        $fileName = $formName . ".csv";
+        header("Content-Disposition: attachment; filename=\"$fileName\"");
+
+        // todo: make this work
+        //$fileName = $this->encodeWordRfc2231($formName) . ".csv";
+        //header("Content-Disposition: attachment; filename*=UTF-8''$fileName");
 
         //Bytes FF FE (UTF-16LE BOM)
         echo chr(255) . chr(254);
@@ -66,6 +71,14 @@ class ExportToCsvUtf16le {
 
     protected function &encode($text) {
         return mb_convert_encoding($text, 'UTF-16LE', 'UTF-8');
+    }
+
+    protected function &encodeWordRfc2231($word) {
+        $binArray = unpack("C*", $word);
+        foreach ($binArray as $chr) {
+            $hex_ary[] = '%' . sprintf("%02X", base_convert($chr, 2, 16));
+        }
+        return implode('', $hex_ary);
     }
 
 }
