b5cef5adcf776127a8b04f680b21824ca50f3ca0
msimpson <msimpson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-12 01:49:01 +0000

    Making IQY authentication to WordPress work
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-to-database-extension/trunk@299156 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ExportToIqy.php
+++ ExportToIqy.php
@@ -8,12 +8,21 @@ class ExportToIqy {
         header("Content-Type: text/x-ms-iqy");
         header("Content-Disposition: attachment; filename=\"$formName.iqy\"");
 
+        $url = get_bloginfo('url');
         $plugin = new CF7DBPlugin();
+        $slug = $plugin->getDBPageSlug();
+
+        // To get this to work right, we have to submit to the same page that the login form does and post
+        // the same parameters that the login form does. This includes "log" and "pwd" for the login and
+        // also "redirect_to" which is the page where we want to end up. We also add the "form_name" parameter
+        // for the final page to select which contact form data is to be displayed.
+        //
+        // the "Selection=3" part references the 3rd table in the page which is the data table.
         echo (
 "WEB
 1
-" . get_bloginfo('url') ."/wp-admin/admin.php?page=" . $plugin->getDBPageSlug() . "
-form_name=$formName&log=[\"WordPress Username\"]&pwd=[\"WordPress Password\"]
+$url/wp-login.php
+form_name=$formName&log=[\"Username for $url\"]&pwd=[\"Password for $url\"]&redirect_to=$url/wp-admin/admin.php?page=$slug
 
 Selection=3
 Formatting=None
