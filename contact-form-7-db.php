e0f310d4f6b75b58fb92085b993c70b4475a06f0
msimpson <msimpson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-03 16:53:44 +0000

    Error handling updates
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-to-database-extension/trunk@404113 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contact-form-7-db.php
+++ contact-form-7-db.php
@@ -2,38 +2,35 @@
 /*
    Plugin Name: Contact Form to DB Extension
    Plugin URI: http://wordpress.org/extend/plugins/contact-form-7-to-database-extension/
-   Version: 1.8.8
+   Version: 1.9
    Author: Michael Simpson
-   Description: Captures form submissions from Contact Form 7 and Fast Secure Contact Form plugins and writes the form data to the database | <a href="admin.php?page=CF7DBPluginSubmissions">Data</a>  | <a href="admin.php?page=CF7DBPluginSettings">Settings</a> | <a href="http://wordpress.org/extend/plugins/contact-form-7-to-database-extension/faq/">FAQ</a>
+   Description: Save form submissions to the database from Contact Form 7 and Fast Secure Contact Form. Includes exports and short codes.  | <a href="admin.php?page=CF7DBPluginSubmissions">Data</a>  | <a href="admin.php?page=CF7DBPluginSettings">Settings</a> | <a href="http://wordpress.org/extend/plugins/contact-form-7-to-database-extension/faq/">FAQ</a>
    Text Domain: contact-form-7-to-database-extension
    License: GPL3
   */
+
+
+$CF7DBPlugin_minimalRequiredPhpVersion = '5.0';
 
 /**
  * Check the PHP version and give a useful error message if the user's version is less than the required version
  * @return boolean true if version check passed. If false, triggers an error which WP will handle, by displaying
  * an error message on the Admin page
  */
-function CF7DBPlugin_PhpVersionCheck() {
-    $minimalRequiredPhpVersion = '5.0';
-    if (version_compare(phpversion(), $minimalRequiredPhpVersion) < 0) {
-        trigger_error(
-            '<p>' . __('Error: Contact Form to DB Plugin requires a newer version of PHP to be running.', 'contact-form-7-to-database-extension') . '</p>' .
-                    '<ul>' .
-                    '<li>' . __('Minimal version of PHP required: ', 'contact-form-7-to-database-extension') . '<strong>' . $minimalRequiredPhpVersion . '</strong></li>' .
-                    '<li>' . __('Your server\'s PHP version: ', 'contact-form-7-to-database-extension') . '<strong>' . phpversion() . '</strong></li>' .
-                    '</ul>' .
+function CF7DBPlugin_noticePhpVersionWrong() {
+    global $CF7DBPlugin_minimalRequiredPhpVersion;
+    echo '<div class="updated fade">' .
+      __('Error: plugin "Contact Form to DB Extension" requires a newer version of PHP to be running.',  'contact-form-7-to-database-extension').
+            '<br/>' . __('Minimal version of PHP required: ', 'contact-form-7-to-database-extension') . '<strong>' . $CF7DBPlugin_minimalRequiredPhpVersion . '</strong></li>' .
+            '<br/>' . __('Your server\'s PHP version: ', 'contact-form-7-to-database-extension') . '<strong>' . phpversion() . '</strong></li>' .
+         '</div>';
+}
 
-                    '<p>' . __('When using the Apache web server, typically you can configure it to use PHP5 by doing the following:', 'contact-form-7-to-database-extension') .
-                    '<ul>' .
-                    '<li>' . __('Locate and edit this file, located at the top directory of your WordPress installation: ', 'contact-form-7-to-database-extension') .
-                    '<strong><code>.htaccess</code></strong></li>' .
-                    '<li>' . __('Add these two lines to the file:', 'contact-form-7-to-database-extension') .
-                    '<br/><code><pre>
-AddType x-mapp-php5 .php
-AddHandler x-mapp-php5 .php
-</pre></code></ul>'
-            , E_USER_ERROR); // E_USER_ERROR seems to be handled OK in WP. It gives a notice in the Plugins Page
+
+function CF7DBPlugin_PhpVersionCheck() {
+    global $CF7DBPlugin_minimalRequiredPhpVersion;
+    if (version_compare(phpversion(), $CF7DBPlugin_minimalRequiredPhpVersion) < 0) {
+        add_action('admin_notices', 'CF7DBPlugin_noticePhpVersionWrong');
         return false;
     }
     return true;
