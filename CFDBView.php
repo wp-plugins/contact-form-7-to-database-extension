29f83f4014b9a4039ffb13f30447c6ad7c750a08
msimpson <msimpson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-26 21:43:23 +0000

    Removal of Plugin Sponsors banner to comply with demand from Automattic
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-to-database-extension/trunk@468001 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- CFDBView.php
+++ CFDBView.php
@@ -39,15 +39,15 @@ abstract class CFDBView {
      * @return void
      */
     protected function sponsorLink(&$plugin) {
-        if ('true' != $plugin->getOption('Donated')) {
-            ?>
-        <script type="text/javascript">
-            var psHost = (("https:" == document.location.protocol) ? "https://" : "http://");
-            document.write(unescape("%3Cscript src='" + psHost + "pluginsponsors.com/direct/spsn/display.php?client=contact-form-7-to-database-extension&spot='type='text/javascript'%3E%3C/script%3E"));
-        </script>
-        <?php
-
-        }
+//        if ('true' != $plugin->getOption('Donated')) {
+//            ?>
+<!--        <script type="text/javascript">-->
+<!--            var psHost = (("https:" == document.location.protocol) ? "https://" : "http://");-->
+<!--            document.write(unescape("%3Cscript src='" + psHost + "pluginsponsors.com/direct/spsn/display.php?client=contact-form-7-to-database-extension&spot='type='text/javascript'%3E%3C/script%3E"));-->
+<!--        </script>-->
+<!--        --><?php
+//
+//        }
     }
 
     protected function headerLinks() {
@@ -55,36 +55,36 @@ abstract class CFDBView {
     <table style="width:100%;">
         <tbody>
         <tr>
-            <td width="20%" style="font-size:x-small;">
+            <td width="25%" style="font-size:x-small;">
                 <a target="_donate"
                    href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=NEVDJ792HKGFN&lc=US&item_name=Wordpress%20Plugin&item_number=cf7%2dto%2ddb%2dextension&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted">
                     <img src="https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif" alt="Donate">
                 </a>
             </td>
-            <td width="20%" style="font-size:x-small;">
+            <td width="25%" style="font-size:x-small;">
                 <a target="_cf7todb"
                    href="http://wordpress.org/extend/plugins/contact-form-7-to-database-extension">
                     <?php _e('Rate this Plugin', 'contact-form-7-to-database-extension') ?>
                 </a>
             </td>
-            <td width="20%" style="font-size:x-small;">
+            <td width="25%" style="font-size:x-small;">
                 <a target="_cf7todb"
                    href="http://cfdbplugin.com/">
                     <?php _e('Documentation', 'contact-form-7-to-database-extension') ?>
                 </a>
             </td>
-            <td width="20%" style="font-size:x-small;">
+            <td width="25%" style="font-size:x-small;">
                 <a target="_cf7todb"
                    href="http://wordpress.org/tags/contact-form-7-to-database-extension">
                     <?php _e('Support', 'contact-form-7-to-database-extension') ?>
                 </a>
             </td>
-            <td width="20%" style="font-size:x-small;">
-                <a target="_cf7todb"
-                   href="http://pluginsponsors.com/privacy.html">
-                    <?php _e('Privacy Policy', 'contact-form-7-to-database-extension') ?>
-                </a>
-            </td>
+<!--            <td width="20%" style="font-size:x-small;">-->
+<!--                <a target="_cf7todb"-->
+<!--                   href="http://pluginsponsors.com/privacy.html">-->
+<!--                    --><?php //_e('Privacy Policy', 'contact-form-7-to-database-extension') ?>
+<!--                </a>-->
+<!--            </td>-->
         </tr>
         </tbody>
     </table>
