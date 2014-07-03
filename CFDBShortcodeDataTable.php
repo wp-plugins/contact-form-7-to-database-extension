bb818b2246819d71237a93861195786783fbdc9f
msimpson <msimpson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-03 15:13:36 +0000

    Bug fix: {{BEFORE}} and {{AFTER}} not working for [cfdb-datatable]
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-to-database-extension/trunk@942952 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- CFDBShortcodeDataTable.php
+++ CFDBShortcodeDataTable.php
@@ -39,7 +39,7 @@ class CFDBShortcodeDataTable extends ShortCodeScriptLoader {
         //     http://beerpla.net/2010/01/13/wordpress-plugin-development-how-to-include-css-and-javascript-conditionally-and-only-when-needed-by-the-posts/
         // But it appears to expects posts on the page and I'm concerned it will not work in all cases
 
-        // Just enqueuing it causes problems in some pages. Need a targetted way to do this. 
+        // Just enqueuing it causes problems in some pages. Need a targeted way to do this.
 //        wp_enqueue_style('datatables-demo', 'http://www.datatables.net/release-datatables/media/css/demo_table.css');
     }
 
