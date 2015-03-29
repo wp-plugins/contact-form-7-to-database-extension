8a76a67acf3c7ed624a6e1128d6034eb610533c0
msimpson <msimpson@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-29 20:53:45 +0000

    Reduced cell padding in admin table but not other datatables
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-to-database-extension/trunk@1123234 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ExportToHtmlTable.php
+++ ExportToHtmlTable.php
@@ -175,7 +175,7 @@ class ExportToHtmlTable extends ExportBase implements CFDBExport {
                 }
 
                 table.<?php echo $this->defaultTableClass ?> th {
-                    padding: 0px;
+                    padding: 5px;
                     border: 1px solid gray;
                 }
 
@@ -185,7 +185,7 @@ class ExportToHtmlTable extends ExportBase implements CFDBExport {
                 }
 
                 table.<?php echo $this->defaultTableClass ?> tbody td {
-                    padding: 0px;
+                    padding: 5px;
                     border: 1px solid gray;
                     font-size: x-small;
                 }
